6d28b2f6b1a7866c8ad1d3f6de505d11db691556
Amanda Pogue <apogue@bcs.rochester.edu>
2016-05-13 10:28:41 -0400

    Changing the style.

--- Qualtrics_Tutorial/README.txt
+++ Qualtrics_Tutorial/README.txt
@@ -1,12 +1,10 @@
-Striped by HTML5 UP
+Arcana by HTML5 UP
 html5up.net | @n33co
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
-And here's a new one: Striped, which features a clean, minimalistic design, styling for
-all basic page elements (including blockquotes, tables and lists), a repositionable
-sidebar (left or right), and HTML5/CSS3 code designed for quick and easy customization
-(see code comments for details).
+A business/corporate style responsive site template. It's pretty barebones but should
+go over pretty well for folks wanting to get their serious business on.
 
 Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
 you can use for pretty much whatever.
@@ -17,6 +15,8 @@ Feedback, bug reports, and comments are not only welcome, but strongly encourage
 
 AJ
 n33.co @n33co dribbble.com/n33
+
+PS: Not sure how to get that contact form working? Give formspree.io a try (it's awesome).
 
 
 Credits:
@@ -31,5 +31,7 @@ Credits:
 		jQuery (jquery.com)
 		html5shiv.js (@afarkas @jdalton @jon_neal @rem)
 		CSS3 Pie (css3pie.com)
+		background-size polyfill (github.com/louisremi)
 		Respond.js (j.mp/respondjs)
+		jquery.dropotron (n33.co)
 		Skel (skel.io)
--- Qualtrics_Tutorial/assets/js/main.js
+++ Qualtrics_Tutorial/assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Striped by HTML5 UP
+	Arcana by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -7,18 +7,18 @@
 (function($) {
 
 	skel.breakpoints({
-		desktop: '(min-width: 737px)',
-		wide: '(min-width: 1201px)',
-		narrow: '(min-width: 737px) and (max-width: 1200px)',
-		narrower: '(min-width: 737px) and (max-width: 1000px)',
-		mobile: '(max-width: 736px)'
+		wide: '(max-width: 1680px)',
+		normal: '(max-width: 1280px)',
+		narrow: '(max-width: 980px)',
+		narrower: '(max-width: 840px)',
+		mobile: '(max-width: 736px)',
+		mobilep: '(max-width: 480px)'
 	});
 
 	$(function() {
 
 		var	$window = $(window),
-			$body = $('body'),
-			$document = $(document);
+			$body = $('body');
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
@@ -30,42 +30,41 @@
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
 
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
+		// Prioritize "important" elements on narrower.
+			skel.on('+narrower -narrower', function() {
 				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
+					'.important\\28 narrower\\29',
+					skel.breakpoint('narrower').active
 				);
 			});
 
-		// Off-Canvas Sidebar.
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				offsetY: -15,
+				hoverDelay: 0,
+				alignment: 'center'
+			});
 
-			// Height hack.
-				var $sc = $('#sidebar, #content'), tid;
-
-				$window
-					.on('resize', function() {
-						window.clearTimeout(tid);
-						tid = window.setTimeout(function() {
-							$sc.css('min-height', $document.height());
-						}, 100);
-					})
-					.on('load', function() {
-						$window.trigger('resize');
-					})
-					.trigger('resize');
+		// Off-Canvas Navigation.
 
 			// Title Bar.
 				$(
 					'<div id="titleBar">' +
-						'<a href="#sidebar" class="toggle"></a>' +
+						'<a href="#navPanel" class="toggle"></a>' +
 						'<span class="title">' + $('#logo').html() + '</span>' +
 					'</div>'
 				)
 					.appendTo($body);
 
-			// Sidebar
-				$('#sidebar')
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						'<nav>' +
+							$('#nav').navList() +
+						'</nav>' +
+					'</div>'
+				)
+					.appendTo($body)
 					.panel({
 						delay: 500,
 						hideOnClick: true,
@@ -74,12 +73,12 @@
 						resetForms: true,
 						side: 'left',
 						target: $body,
-						visibleClass: 'sidebar-visible'
+						visibleClass: 'navPanel-visible'
 					});
 
 			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
 				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#titleBar, #sidebar, #main')
+					$('#titleBar, #navPanel, #page-wrapper')
 						.css('transition', 'none');
 
 	});
